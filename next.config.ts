1bd350e56e7e85639fe584b7a5c4a3508dbf5ad7
mrrbrilliant <mrrbrilliantwork@gmail.com>
2025-04-30 16:29:22 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,13 @@ const nextConfig: NextConfig = {
 		return [
 			{
 				source: "/api/:path*",
-				destination: "http://localhost:4000/:path*", // Proxy to Backend
+				// Use environment variable for backend URL, default to localhost:4000
+				// preiiter
+				destination: `${process.env.BACKEND_URL}/:path*`,
 			},
 		];
 	},
+	output: "standalone",
 };
 
 export default nextConfig;
